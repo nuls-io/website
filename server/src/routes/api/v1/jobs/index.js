25a73b806579c8f0d963245578c6c93054384be1
wave <986887991@qq.com>
2018-10-23 08:56:46 +0800

    new websitew

--- server/src/routes/api/v1/jobs/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const jobs = require('@/controllers/jobs');
-
-const router = express.Router();
-
-router.get('/', jobs.getActive);
-
-module.exports = router;
--- server/src/routes/api/v1/partners/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const partners = require('@/controllers/partners');
-
-const router = express.Router();
-
-router.get('/', partners.getEverything);
-
-module.exports = router;
